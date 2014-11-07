317db2b18e20d3cb86b09dbeaafb7bc9602947c7
Nils Windisch <mail@nilswindisch.de>
2014-11-07 08:58:36 +0100

    [ADD] IPOA-192 grunt tasks: cssmin and uncss

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -6,5 +6,13 @@ module.exports = {
     files: {
       '_site/css/start.css': ['_site/css/start.css']
     }
+  },
+  style: {
+    options: {
+      keepSpecialComments: '0'
+    },
+    files: {
+      '_site/css/style.css': ['_site/css/style.css']
+    }
   }
 };
--- grunt/uncss.js
+++ grunt/uncss.js
@@ -6,5 +6,13 @@ module.exports = {
     files: {
       '_site/css/start.css': ['_site/start-1.html']
     }
+  },
+  style: {
+    options: {
+      report: 'min'
+    },
+    files: {
+      '_site/css/style.css': ['_site/content-1.html']
+    }
   }
 };
