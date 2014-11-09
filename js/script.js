d9d48c87c067c57f3f3bdf54bdc39e838441ff3e
Nils Windisch <mail@nilswindisch.de>
2014-11-09 19:54:06 +0100

    [UPDATE] IPOA-192 JS for hidden footer and nav

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  // footerHide
+  footerHideHeight = $('.footer-hide__content').height();
+  $('.footer-hide').css({"height": footerHideHeight + "px"});
+
+  // nav
+  nav = $('.js-nav');
+  navToggleButton = $('.js-nav-toggle-button');
+  navToggleButtonIcon = $('.js-menu-button-icon');
+  $(navToggleButton).click(function() {
+    if (navToggleButton.hasClass('js-active')) {
+      $(navToggleButton).removeClass('js-active');
+      $(navToggleButtonIcon).removeClass('fa-times');
+      $(navToggleButtonIcon).addClass('fa-bars');
+      $(nav).slideUp();
+    } else {
+      $(navToggleButton).addClass('js-active');
+      $(navToggleButtonIcon).removeClass('fa-bars');
+      $(navToggleButtonIcon).addClass('fa-times');
+      $(nav).slideDown();
+    }
+  });
+
+});
