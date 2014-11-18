05a370a216985ab8824cd7cbbf7e9b7a7343c80e
Nils Windisch <mail@nilswindisch.de>
2014-11-18 15:32:33 +0100

    [UPDATE] moving header and some padding/margin fixes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
 
   /**
    * TODO : add documentation
+   */
+  function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  }
+
+  /**
+   * TODO : add documentation
    * 
    * footerHide
    */
@@ -12,12 +20,14 @@ $(document).ready(function () {
   $('.footer-hide').css({ 'height': footerHideHeight + 'px' });
 
   /**
-   *
+   * TODO : add documentation
    */
   jsHeadLanguageIndicator = $('.js-head-language-indicator');
   jsHeadLanguageIndicator.click(function() {
     jsHeadLanguageIndicator.hide();
     $('.js-head-language-links').show();
+    headHeight = $('.js-head').height();
+    $('.headroom__helper').css({"padding-top": headHeight + "px"});
   });
 
   /**
@@ -29,15 +39,34 @@ $(document).ready(function () {
   $(menuToggleButton).click(function() {
     event.preventDefault();
     if (menuToggleButton.hasClass('js-active')) {
+      $('html,body').animate({'scrollTop' : 0},500);
       $(menuToggleButton).removeClass('js-active');
       $(menuToggleButtonIcon).removeClass('fa-times');
       $(menuToggleButtonIcon).addClass('fa-bars');
       $(menu).slideUp();
     } else {
+      $('html,body').animate({'scrollTop' : 0},500);
       $(menuToggleButton).addClass('js-active');
       $(menuToggleButtonIcon).removeClass('fa-bars');
       $(menuToggleButtonIcon).addClass('fa-times');
       $(menu).slideDown();
+    }
+  });
+
+  /**
+   * TODO : add documentation
+   * JS to make the font-size of the claim shown in the header larger
+   */
+  headHeight = $('.js-head').height();
+  $('.headroom__helper').css({"padding-top": headHeight + "px"});
+  $('.js-head').headroom({
+    offset : headHeight,
+    classes : {
+      initial : "js-head",
+      pinned : "js-head--pinned",
+      unpinned : "js-head--unpinned",
+      top : "js-head--top",
+      notTop : "js-head--not-top"
     }
   });
 
